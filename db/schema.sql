2d3dad837cd39879ca1d8f9282bdc0a9816fe0aa
noellecrow <crownoelle@gmail.com>
2022-03-20 17:06:46 -0700

    created tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,29 @@
-CREATE TABLE departments (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+USE company_db;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL (11,2) NOT NULL,
+    department_id INT,
+    INDEX dep_ind (department_id),
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    INDEX role_ind (role_id),
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    manager_id INT,
+    INDEX manager_ind (manager_id),
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -3,5 +3,14 @@ VALUES
     ('Sales'),
     ('Engineering'),
     ('Finance'),
-    ('Legal'),
-    ('Service');
+    ('Legal');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+    ('Sales Lead', 90000, 1),
+    ('Sales Team Member', 60000, 1),
+    ('Junior Software Engineer', 96000, 2),
+    ('Senior Software Engineer', 150000, 2),
+    ('Junior Financial Advisor', 100000, 3),
+    ('Senior Financial Advisor', 200000, 3),
+    ('Legal Advisor', 2500000, 4);
